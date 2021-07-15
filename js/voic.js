4e9ad7c2538180c5c22033dfad7dd1b74a9f1f79
Baluds <baluds1999@gmail.com>
2021-07-15 17:28:35 +0530

    Added welcome note to pages
    changed spelling mistake
    
    added view to grammar

--- js/voic.js
+++ js/voic.js
@@ -2,7 +2,7 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var cats = [ 'men' , 'women' , 'computer' ,'home' , 'contact' ];
+var cats = [ 'men' , 'women' , 'computer' ,'home' , 'contact' ,'view' ];
 var grammar = '#JSGF V1.0; grammar cats; public <color> = ' + cats.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
@@ -23,9 +23,9 @@ recognition.continuous = true;
 recognition.onresult = function(event) {
 
   var cat = event.results[0][0].transcript;
-  if(cat=='home' || cat=='man' || cat=='computers' || cat=='men'|| cat=='women' || cat=='computer'|| cat=='contact')
+  if(cat=='home' || cat=='man' || cat=='computers' || cat=='men'|| cat=='women' || cat=='computer'|| cat=='contact' || cat=='view')
   {
-    if(cat=='home')
+    if(cat=='home'|| cat=='view')
     {
         cat='index';
     }
